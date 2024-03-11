0233885ef9771bbff227aad3b598406f79539484
ciara <csmobile99@gmail.com>
2024-03-11 15:52:08 -0700

    Create a web view

---
+++ Sources/LightplaySDKPackage/LightplaySDKPackage.swift
@@ -0,0 +1,30 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Foundation
+import UIKit
+import WebKit
+
+public class MediumWebView: WKWebView {
+    
+    public init() {
+        let webConfiguration = WKWebViewConfiguration()
+        webConfiguration.preferences.javaScriptEnabled = true
+        super.init(frame: .zero, configuration: webConfiguration)
+        self.scrollView.isScrollEnabled = false
+        self.isMultipleTouchEnabled = false
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    @discardableResult
+    public func load() -> WKNavigation? {
+        if let url = URL(string:"http://example.com") {
+            let req = URLRequest(url: url)
+            return super.load(req)
+        }
+        return nil
+    }
+}
