1f89751589ff1848f649fef581986d5d80ec7962
ciara <csmobile99@gmail.com>
2024-03-12 17:42:23 -0700

    modifying the Package.swift

--- Package.swift
+++ Package.swift
@@ -11,6 +11,9 @@ let package = Package(
             name: "LightplaySDKPackage",
             targets: ["LightplaySDKPackage"]),
     ],
+//    dependencies: [
+//        .package(url: "https://github.com/rypplzzdev/LightplaySDKPackage.git", branch:"main")
+//    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
